f9a87ae971a36352ad0b0764d95ea47234cb715b
Jori Bomanson <jori.bomanson@aalto.fi>
2016-08-17 13:27:39 +0300

    Use I for the numeric type parameter in OEMerge

--- src/pbtranslator/scheme/oe_merge.cr
+++ src/pbtranslator/scheme/oe_merge.cr
@@ -13,10 +13,10 @@ module PBTranslator
   # Both sequences must have the same width and it must be a power of two.
   class Scheme::OEMerge
 
-    record Network(T), half_width_log2 : T do
+    record Network(I), half_width_log2 : I do
 
       def size
-        (T.new(1) << half_width_log2) * half_width_log2 + 1
+        (I.new(1) << half_width_log2) * half_width_log2 + 1
       end
 
       def depth
@@ -26,15 +26,15 @@ module PBTranslator
       # :nodoc:
       macro define_visit(prefix, each_expr)
 
-        def {{prefix.id}}visit(visitor, offset = T.new(0))
-          a, b = {T.new(0), half_width_log2}
+        def {{prefix.id}}visit(visitor, offset = I.new(0))
+          a, b = {I.new(0), half_width_log2}
           {{each_expr}} do |layer_index|
             {{prefix.id}}layer_visit(visitor, offset, layer_index)
           end
         end
 
         private def {{prefix.id}}layer_visit(visitor, offset, layer_index)
-          a, b = {T.new(0), (T.new(1) << (half_width_log2 + 1)) - 1}
+          a, b = {I.new(0), (I.new(1) << (half_width_log2 + 1)) - 1}
           {{each_expr}} do |wire_index|
             partner_index = partner(half_width_log2, layer_index, wire_index)
             next unless wire_index < partner_index
@@ -55,13 +55,13 @@ module PBTranslator
       define_visit reverse_, b.downto(a)
 
       private def partner(half_width_log2, layer_index, wire_index)
-        if T.new(0) == layer_index
-          wire_index ^ (T.new(1) << half_width_log2)
+        if I.new(0) == layer_index
+          wire_index ^ (I.new(1) << half_width_log2)
         else
           r = half_width_log2 - layer_index
           s = wire_index >> r
 
-          if {T.new(0), (T.new(1) << (layer_index + 1)) - 1}.includes? s
+          if {I.new(0), (I.new(1) << (layer_index + 1)) - 1}.includes? s
             wire_index # No comparator for this wire at this level.
           else
             wire_index - (1 - 2 * (s % 2)) * (1 << r)
