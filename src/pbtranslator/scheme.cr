e7c96e6a1ee7b141438866a7232316139a447c76
Jori Bomanson <jori.bomanson@aalto.fi>
2016-08-18 11:33:42 +0300

    Add a file for the Scheme module

---
+++ src/pbtranslator/scheme.cr
@@ -0,0 +1,6 @@
+# This module contains schemes for generating networks.
+#
+# Each scheme represents some class of networks parametrized by some property.
+# These networks can generally be instantiated for given parameter values by
+# calling `network` methods.
+module PBTranslator::Scheme end
