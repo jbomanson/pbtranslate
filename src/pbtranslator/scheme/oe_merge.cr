e3bce9a79df03797d431c86876be288c0665c896
Jori Bomanson <jori.bomanson@aalto.fi>
2016-09-06 12:49:06 +0300

    Rename *Scheme::OEMerge::Network
    ... into *Network::OEMerge and palce it into a file of its own.

--- src/pbtranslator/scheme/oe_merge.cr
+++ src/pbtranslator/scheme/oe_merge.cr
@@ -1,3 +1,5 @@
+require "../network/oe_merge"
+
 module PBTranslator
 
   # An OEMerge scheme represents Batcher's odd-even merging networks.
@@ -7,86 +9,18 @@ module PBTranslator
   # Instances of these networks are obtained via `#network`.
   class Scheme::OEMerge
 
-    # See `OEMerge`.
-    struct Network(I)
-
-      # The binary logarithm of the width of the input halves of this network.
-      getter half_width_log2
-
-      # See `OEMerge#network`.
-      def initialize(@half_width_log2 : I)
-      end
-
-      # Returns the number of comparators in the network.
-      def size
-        (I.new(1) << half_width_log2) * half_width_log2 + 1
-      end
-
-      # Returns the number of comparators on the longest path from an input to
-      # an output.
-      def depth
-        half_width_log2 + 1
-      end
-
-      # :nodoc:
-      macro define_visit(prefix, each_expr)
-        def {{prefix.id}}visit(visitor, offset = I.new(0))
-          a, b = {I.new(0), half_width_log2}
-          {{each_expr}} do |layer_index|
-            {{prefix.id}}layer_visit(visitor, offset, layer_index)
-          end
-        end
-
-        private def {{prefix.id}}layer_visit(visitor, offset, layer_index)
-          a, b = {I.new(0), (I.new(1) << (half_width_log2 + 1)) - 1}
-          {{each_expr}} do |wire_index|
-            partner_index = partner(half_width_log2, layer_index, wire_index)
-            next unless wire_index < partner_index
-            comparator = Comparator.new(wire_index, partner_index)
-            comparator = comparator.shifted by: offset
-            visitor.{{prefix.id}}visit(comparator)
-          end
-        end
-      end
-
-      # Performs a visit over the comparators in this network placed at an
-      # *offset*.
-      #
-      # The visit method of *visitor* is called for each comparator.
-      define_visit "", a.upto(b)
-
-      # Like `#visit` but in reverse order and calling reverse_visit instead.
-      define_visit reverse_, b.downto(a)
-
-      private def partner(half_width_log2, layer_index, wire_index)
-        if I.new(0) == layer_index
-          wire_index ^ (I.new(1) << half_width_log2)
-        else
-          r = half_width_log2 - layer_index
-          s = wire_index >> r
-
-          if {I.new(0), (I.new(1) << (layer_index + 1)) - 1}.includes? s
-            wire_index # No comparator for this wire at this level.
-          else
-            wire_index - (1 - 2 * (s % 2)) * (1 << r)
-          end
-        end
-      end
-
-    end
-
     # The only instance of the OEMerge scheme that needs to be used.
     #
-    # To obtain instances of OEMerge `Network`s, use `#network`.
+    # To obtain instances of networks, use `#network`.
     INSTANCE = self.new
 
-    # Returns a `Network` instance for merging pairs of consecutive sequences of
-    # of the same width that is a power of two.
+    # Returns a `Network::OEMerge` instance for merging pairs of consecutive
+    # sequences of of the same width that is a power of two.
     #
     # The binary logarithm of the width of both of these halves is
     # *half_width_log2*.
     def network(half_width_log2)
-      Network.new(half_width_log2)
+      Network::OEMerge.new(half_width_log2)
     end
 
   end
