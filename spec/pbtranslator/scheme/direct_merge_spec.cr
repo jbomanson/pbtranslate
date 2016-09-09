fe3c423131cd2e1d0b0b36656b452cdc689aae92
Jori Bomanson <jori.bomanson@aalto.fi>
2016-09-09 10:27:30 +0300

    Improve the DirectMerge spec

--- spec/pbtranslator/scheme/direct_merge_spec.cr
+++ spec/pbtranslator/scheme/direct_merge_spec.cr
@@ -1,27 +1,30 @@
 require "../../spec_helper"
 
-L = 1
-N = 1 << L
+half_width_log2_max = 3
+half_width_max = 1 << half_width_log2_max
 
 scheme = PBTranslator::Scheme::DirectMerge::INSTANCE
 
 describe typeof(scheme) do
-  it "represents a logic network that merges #{N}^2 booleans" do
-    # Generate all sorted arrays of N booleans.
-    x =
-      Array.new(N + 1) do |i|
-        Array.new(N) do |j|
-          !(i <= j)
+  it "represents a network that merges up to #{half_width_max}^2 booleans" do
+    (0..half_width_log2_max).each do |half_width_log2|
+      half_width = 1 << half_width_log2
+      # Generate all sorted arrays of N booleans.
+      x =
+        Array.new(half_width + 1) do |i|
+          Array.new(half_width) do |j|
+            j < i
+          end
         end
+      # Enumerate pairs of sorted pairs.
+      x.product(x) do |u, v|
+        a = u + v
+        b = a.clone
+        c = a.sort_by {|w| w ? 0 : 1}
+        visitor = PBTranslator::Visitor::ArrayLogic.new(b, false)
+        scheme.network(half_width_log2).visit(visitor)
+        b.should eq(c)
       end
-    # Enumerate pairs of sorted pairs.
-    x.product(x) do |u, v|
-      a = u + v
-      b = a.clone
-      c = a.sort_by {|w| w ? 0 : 1}
-      visitor = PBTranslator::Visitor::ArrayLogic.new(b, false)
-      scheme.network(L).visit(visitor)
-      b.should eq(c)
     end
   end
 end
