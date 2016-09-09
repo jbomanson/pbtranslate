c3458b819f572928ae9931045e95d88a763c2b74
Jori Bomanson <jori.bomanson@aalto.fi>
2016-09-09 10:07:18 +0300

    Add a simple spec for LagArray

---
+++ spec/pbtranslator/lag_array_spec.cr
@@ -0,0 +1,15 @@
+require "../spec_helper"
+
+describe PBTranslator::LagArray do
+
+  it "can swap a pair in the middle of an array" do
+    a = [:a, :b, :c, :d]
+    g = PBTranslator::LagArray.new(a)
+    g.lag do |d|
+      d[1] = d[2]
+      d[2] = d[1]
+    end
+    a.should eq([:a, :c, :b, :d])
+  end
+
+end
