774025cd9dd1941173eb0f774b32ccb7c07a6976
Jori Bomanson <jori.bomanson@aalto.fi>
2016-09-30 15:06:01 +0300

    Add IdBroker

---
+++ spec/pbtranslator/util/id_broker_spec.cr
@@ -0,0 +1,51 @@
+require "../../spec_helper"
+
+medium_number = 10_000
+acceptable_size_factor = 1.20
+seed = SEED ^ __FILE__.hash
+
+describe PBTranslator::Util::IdBroker do
+  it "generates 0, 1, ..., #{medium_number}" do
+    broker = PBTranslator::Util::IdBroker.new
+    medium_number.times do |i|
+      broker.fresh_id.should eq(i)
+    end
+  end
+
+  it "maps a random permutation of S = {0, 1, ..., #{medium_number}} to S" do
+    random = Random.new(seed)
+    broker = PBTranslator::Util::IdBroker.new
+    a = Array.new(medium_number, &.itself)
+    b = a.shuffle(random)
+    c =
+      Array.new(medium_number) do |i|
+        broker.rename(b[i])
+      end
+    c.sort!
+    c.each_index do |i|
+      {i, c[i]}.should eq({i, a[i]})
+    end
+  end
+
+  it "generates and renames compactly and without conflicts" do
+    random = Random.new(seed)
+    broker = PBTranslator::Util::IdBroker.new
+    a =
+      Array.new(2 * medium_number) do |i|
+        {i.odd?, i / 2}
+      end
+    a.shuffle! random
+    b =
+      a.map do |(rename_please, index)|
+        if rename_please
+          broker.rename(index)
+        else
+          broker.fresh_id
+        end
+      end
+    b.sort!
+    b.should eq(b.uniq)
+    acceptable_size = 2 * medium_number * acceptable_size_factor
+    b.max.should be < acceptable_size
+  end
+end
