180d5c9ba06c20d986368a5ae95257ab09a7ee29
Jori Bomanson <jori.bomanson@aalto.fi>
2016-12-16 14:09:56 +0200

    In Way, improve documentation

--- src/pbtranslator/way.cr
+++ src/pbtranslator/way.cr
@@ -1,44 +1,98 @@
-module PBTranslator
-  FORWARD  = Forward.new
-  BACKWARD = Backward.new
+# An abstract struct with two descendants, `Backward` and `Forward`
+# with corresponding instances `BACKWARD` and `FORWARD`.
+#
+# These structs hold no data.
+# They facilitate iteration in parametrized directions.
+#
+# ### Example
+#
+#     include PBTranslator
+#
+#     def join(way : Way)
+#       a = [100, 200, 300]
+#       String.build do |builder|
+#         way.each_in(a) do |n|
+#           builder << '('
+#           builder << n
+#           builder << ')'
+#         end
+#       end
+#     end
+#
+#     join(FORWARD)	# => "(100)(200)(300)"
+#     join(BACKWARD)	# => "(300)(200)(100)"
+abstract struct PBTranslator::Way
+  # Iterates over the integers in the given inclusive range,
+  # passing each in turn to the given block.
+  #
+  # The order of iteration is determined by the type of _self_.
+  def each_between(lo, hi)
+    yield nil
+  end
 
-  abstract struct Way
-    macro define_each
-      def each
-        each_in(FORWARD) { |x| yield x }
-      end
+  # Similar to Enumerable#each for _object_.
+  #
+  # The order of iteration is determined by the type of _self_.
+  def each_in(object)
+    yield nil
+  end
 
-      def reverse_each
-        each_in(BACKWARD) { |x| yield x }
-      end
+  # Similar to Indexable#each_index for _object_.
+  #
+  # The order of iteration is determined by the type of _self_.
+  def each_index_to(object)
+    yield nil
+  end
+
+  # A convenience macro for defining argumentless _each_ and *reverse_each*
+  # methods for an object that defines *each_in* with a single `Way` argument.
+  macro define_each
+    def each
+      each_in(FORWARD) { |x| yield x }
+    end
+
+    def reverse_each
+      each_in(BACKWARD) { |x| yield x }
     end
   end
 
-  struct Forward < Way
-    def each_between(lo, hi)
-      lo.upto(hi) { |x| yield x }
+  # A module containing the definitions of the descendants of `Way`.
+  #
+  # For convenience, this module is included in the PBTranslator module.
+  module Module
+    module ::PBTranslator
+      include Way::Module
     end
 
-    def each_in(object)
-      object.each { |x| yield x }
+    FORWARD  = Forward.new
+    BACKWARD = Backward.new
+
+    struct Forward < Way
+      def each_between(lo, hi)
+        lo.upto(hi) { |x| yield x }
+      end
+
+      def each_in(object)
+        object.each { |x| yield x }
+      end
+
+      def each_index_to(object)
+        each_between(0, object.size - 1) { |x| yield x }
+      end
     end
 
-    def each_index_to(object)
-      each_between(0, object.size - 1) { |x| yield x }
-    end
-  end
+    struct Backward < Way
+      def each_between(lo, hi)
+        hi.downto(lo) { |x| yield x }
+      end
 
-  struct Backward < Way
-    def each_between(lo, hi)
-      hi.downto(lo) { |x| yield x }
-    end
+      def each_in(object)
+        object.reverse_each { |x| yield x }
+      end
 
-    def each_in(object)
-      object.reverse_each { |x| yield x }
-    end
-
-    def each_index_to(object)
-      each_between(object.size - 1, 0) { |x| yield x }
+      def each_index_to(object)
+        each_between(object.size - 1, 0) { |x| yield x }
+      end
     end
   end
 end
