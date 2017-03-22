a2eee83ff8265a394157663af1dd20138a71ca5c
Jori Bomanson <jori.bomanson@aalto.fi>
2017-03-22 16:00:09 +0200

    Clean Network::WidthLimited from unused code

--- src/pbtranslator/network/width_limited.cr
+++ src/pbtranslator/network/width_limited.cr
@@ -1,5 +1,4 @@
 struct PBTranslator::Network::WidthLimited(N)
-
   delegate network_depth, network_read_count, to: @network
 
   def initialize(@network : N, @width : Distance)
@@ -25,12 +24,6 @@ struct PBTranslator::Network::WidthLimited(N)
     def initialize(@visitor : V, @width : Distance)
     end
 
-    def visit_gate(g : Gate(Or, Input, _), *args, **options) : Nil
-      limited = typeof(g).new?(Wires.new(wires, &.<(@width)))
-      return unless limited
-      @visitor.visit_gate(limited, *args, **options)
-    end
-
     macro define_visit_gate(please_yield)
       def visit_gate(g : Gate(_, Output, _) | Gate(_, InPlace, _) | Gate(And, _, _), **options) : Nil
         return unless g.wires.all? &.<(@width)
@@ -45,24 +38,6 @@ struct PBTranslator::Network::WidthLimited(N)
 
     def visit_region(region) : Nil
       @visitor.visit_region(region) { |v| yield Guide.new(v, @width) }
-    end
-
-    private struct Wires(T, P)
-      def self.new?(wires)
-        picks = wires.map { |w| yield(w) }
-        picks.any? ? new(wires, picks) : nil
-      end
-
-      def initialize(@wires : T, @picks : P)
-      end
-
-      Way.define_each
-
-      private def each_in(way)
-        way.each_index_to(@wires) do |i|
-          @picks[i] && (yield @wires[i])
-        end
-      end
     end
   end
 end
