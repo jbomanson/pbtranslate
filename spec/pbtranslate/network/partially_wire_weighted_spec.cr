4162acae41645065acfc22b24023126797f31397
Bomanson Jori <jori.bomanson@aalto.fi>
2017-09-18 16:04:36 +0300

    In PartiallyWireWeighted spec test corner cases

--- spec/pbtranslate/network/partially_wire_weighted_spec.cr
+++ spec/pbtranslate/network/partially_wire_weighted_spec.cr
@@ -151,6 +151,19 @@ def weight_grid_test(comparators, depth, initial_weights, layer_cache_class, bit
   v.grid.should eq(expected_final_weights)
 end
 
+def corner_case_weight_test_helper(scheme, layer_cache_class, random, weight_range, width_value, bit_value, last_bit_value)
+  width = Width.from_value(width_value)
+  weights = Array.new(width_value) { random.rand(weight_range) }
+  visitor = WireWeightCollectingVisitor(typeof(weights.first)).new(width_value)
+  n = scheme.network(width)
+  bit_array = BitArray.new(n.network_depth.to_i, bit_value)
+  bit_array[-1] = last_bit_value
+  nn = layer_cache_class.new(network: n, width: width)
+  nnn = Network::PartiallyWireWeighted.new(network: nn, bit_array: bit_array, weights: weights.clone)
+  nnn.host(visitor.going(FORWARD))
+  {weights, visitor.grid}
+end
+
 describe Network::PartiallyWireWeighted do
   it "preserves sums of weights placed on all layers when going forward" do
     random = Random.new(seed)
@@ -259,5 +272,31 @@ describe Network::PartiallyWireWeighted do
       [0, 0, 1, 0],
     ]
     weight_grid_test(comparators, depth, initial_weights, layer_cache_class, bit_array, expected_final_weights)
+  end
+
+  it "propagates nothing when given a false bit array" do
+    random = Random.new(seed)
+    random_width_array(network_count, random).each do |width_value|
+      weights, grid = corner_case_weight_test_helper(scheme, layer_cache_class, random, weight_range, width_value, false, false)
+      grid.map(&.first).should eq(weights)
+      grid.each do |single_wire_weights|
+        t = single_wire_weights[1..-1]
+        t.should eq(t.map { 0 })
+      end
+    end
+  end
+
+  it "propagates in one step over everything when given a bit array with a single true bit at the end" do
+    random = Random.new(seed)
+    random_width_array(network_count, random).each do |width_value|
+      weights, grid = corner_case_weight_test_helper(scheme, layer_cache_class, random, weight_range, width_value, false, true)
+      least = weights.min
+      grid.map(&.first).should eq(weights.map &.-(least))
+      grid.map(&.last).should eq(weights.map { least })
+      grid.each do |single_wire_weights|
+        t = single_wire_weights[1..-2]
+        t.should eq(t.map { 0 })
+      end
+    end
   end
 end
