33ab58df22bf5fd02e59f0117471dce3ece97595
Bomanson Jori <jori.bomanson@aalto.fi>
2017-11-23 18:13:19 +0200

    Implement Empty#network_*
    Namely, implement #network_depth, #network_width, #network_write_count.

--- src/pbtranslate/network/empty.cr
+++ src/pbtranslate/network/empty.cr
@@ -1,6 +1,16 @@
 struct PBTranslate::Network::Empty
   INSTANCE = new
 
+  {% for message_and_type in [
+      {:network_depth, Distance},
+      {:network_width, Distance},
+      {:network_write_count, Area},
+  ] %}
+    def {{message_and_type[0].id}} : {{message_and_type[1]}}
+      {{message_and_type[1]}}.new(0)
+    end
+  {% end %}
+
   def host(visitor) : Nil
   end
 end
