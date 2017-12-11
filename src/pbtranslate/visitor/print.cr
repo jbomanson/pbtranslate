0354864fabc80c5306047954dcafd3793b849e82
Jori Bomanson <jori.bomanson@aalto.fi>
2017-12-11 23:50:31 +0200

    Fix Visitor::Print by removing an impossible restriction

--- src/pbtranslate/visitor/print.cr
+++ src/pbtranslate/visitor/print.cr
@@ -1,11 +1,12 @@
 struct PBTranslate::Visitor::Print
+  include Gate::Restriction
   include Visitor
   include Visitor::DefaultMethods
 
   def initialize(@io : IO)
   end
 
-  def visit_gate(g : Gate::Comparator, *empty_args, depth, **options) : Nil
+  def visit_gate(g : Gate(Comparator, InPlace, _), *empty_args, depth, **options) : Nil
     i, j = g.wires
     @io.puts "comparator(#{i}, #{j}, #{depth})."
   end
