3ad55f11622f3910629d92619009a9c868752abb
Jori Bomanson <jori.bomanson@aalto.fi>
2017-12-11 22:03:40 +0200

    Turn sorting_network_helper macros to methods

--- spec/pbtranslate/sorting_network_helper.cr
+++ spec/pbtranslate/sorting_network_helper.cr
@@ -3,29 +3,24 @@ require "../../spec_helper"
 include PBTranslate
 include Gate::Restriction
 
-macro it_hosts_like_a_sorting_network(scheme, seed, range, rounds)
-  %scheme = {{scheme}}
-  %seed = {{seed}}
-  %range = {{range}}
-  %rounds = {{rounds}}
-
+def it_hosts_like_a_sorting_network(scheme, seed, range, rounds)
   it "represents some networks that sort" do
-    random = Random.new(%seed)
-    %range.each do |width|
-      %rounds.times do
+    random = Random.new(seed)
+    range.each do |width|
+      rounds.times do
         a = Array.new(width.value) { random.rand }
         b = a.clone
         c = a.sort
         visitor = Visitor::ArraySwap.new(b)
-        %scheme.network(width).host(visitor)
+        scheme.network(width).host(visitor)
         {a, b}.should eq({a, c})
       end
     end
   end
 
   it "represents matching numbers of gates going forward and backward" do
-    %range.each do |width|
-      network = %scheme.network(width)
+    range.each do |width|
+      network = scheme.network(width)
 
       vf, vb = Array.new(2) { VisitCallCounter.new }
       wf, wb = {FORWARD, BACKWARD}
@@ -41,27 +36,22 @@ macro it_hosts_like_a_sorting_network(scheme, seed, range, rounds)
   end
 end
 
-macro it_reports_like_a_sorting_network(scheme, seed, range, rounds)
-  %scheme = {{scheme}}
-  %seed = {{seed}}
-  %range = {{range}}
-  %rounds = {{rounds}}
-
+def it_reports_like_a_sorting_network(scheme, seed, range, rounds)
   it "returns type Distance for #network_depth" do
-    typeof(%scheme.network(%range.first).network_depth).should eq(Distance)
+    typeof(scheme.network(range.first).network_depth).should eq(Distance)
   end
 
   it "returns type Distance for #network_width" do
-    typeof(%scheme.network(%range.first).network_width).should eq(Distance)
+    typeof(scheme.network(range.first).network_width).should eq(Distance)
   end
 
   it "returns type Area for #network_write_count" do
-    typeof(%scheme.network(%range.first).network_write_count).should eq(Area)
+    typeof(scheme.network(range.first).network_write_count).should eq(Area)
   end
 
   it "returns consistent #network_write_count values" do
-    %range.each do |width|
-      network = %scheme.network(width)
+    range.each do |width|
+      network = scheme.network(width)
       a = network.network_write_count
       visitor = VisitCallCounter.new
       network.host(visitor)
@@ -71,16 +61,16 @@ macro it_reports_like_a_sorting_network(scheme, seed, range, rounds)
   end
 
   it "returns consistent #network_depth values" do
-    %range.each do |width|
-      network = %scheme.network(width)
+    range.each do |width|
+      network = scheme.network(width)
       a = network.network_depth
-      b = %scheme.compute_depth(width)
+      b = scheme.compute_depth(width)
       a.should eq(b)
     end
   end
 end
 
-macro it_acts_like_a_sorting_network(scheme, seed, range, rounds)
-  it_hosts_like_a_sorting_network({{scheme}}, {{seed}}, {{range}}, {{rounds}})
-  it_reports_like_a_sorting_network({{scheme}}, {{seed}}, {{range}}, {{rounds}})
+def it_acts_like_a_sorting_network(scheme, seed, range, rounds)
+  it_hosts_like_a_sorting_network(scheme, seed, range, rounds)
+  it_reports_like_a_sorting_network(scheme, seed, range, rounds)
 end
