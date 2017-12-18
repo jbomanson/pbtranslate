5bd00de1645d2836a6ffd10ec83abc61a91e3791
Bomanson Jori <jori.bomanson@aalto.fi>
2017-12-18 22:39:02 +0200

    Simplify Pw2MergeOddEven spec

--- spec/pbtranslate/scheme/pw2_merge_odd_even_spec.cr
+++ spec/pbtranslate/scheme/pw2_merge_odd_even_spec.cr
@@ -1,22 +1,15 @@
 require "./merging_network_helper"
 
-module Pw2MergeOddEvenSpec
-  extend self
+private SCHEME = Scheme.pw2_merge_odd_even
 
-  SCHEME = Scheme.pw2_merge_odd_even
+private def create_network(i, j)
+  SCHEME.network(Width.from_pw2(Distance.new(i)))
+end
 
-  def create_network(i, j)
-    SCHEME.network(Width.from_pw2(Distance.new(i)))
-  end
-
-  describe Scheme::Pw2MergeOddEven do
-    {% begin %}
-      {% a = [1, 2, 4, 8, 16, 32] %}
-      {% for l in a %}
-        it_merges({{l}}, {{l}}, Visitor::ArrayLogic) do |i, j|
-          create_network(i, j)
-        end
-      {% end %}
-    {% end %}
+describe Scheme::Pw2MergeOddEven do
+  [1, 2, 4, 8, 16, 32].each do |half_width|
+    it_merges(half_width, half_width, Visitor::ArrayLogic) do |i, j|
+      create_network(i, j)
+    end
   end
 end
