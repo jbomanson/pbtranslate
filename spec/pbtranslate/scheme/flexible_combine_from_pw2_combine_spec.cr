abb5fd0b4d2dc166fddada36059074d2dc769ab2
Bomanson Jori <jori.bomanson@aalto.fi>
2017-12-18 22:41:19 +0200

    Simplify FlexibleCombineFromPw2Combine spec

--- spec/pbtranslate/scheme/flexible_combine_from_pw2_combine_spec.cr
+++ spec/pbtranslate/scheme/flexible_combine_from_pw2_combine_spec.cr
@@ -1,24 +1,18 @@
 require "./merging_network_helper"
 
-module FlexibleCombineFromPw2CombineSpec
-  extend self
+private SCHEME = Scheme.pw2_merge_odd_even.to_scheme_flexible_combine
 
-  SCHEME = Scheme.pw2_merge_odd_even.to_scheme_flexible_combine
+private def create_network(*args)
+  SCHEME.network(args.map { |t| Width.from_value(Distance.new(t)) })
+end
 
-  def create_network(*args)
-    SCHEME.network(args.map { |t| Width.from_value(Distance.new(t)) })
-  end
-
-  describe Scheme::FlexibleCombineFromPw2Combine do
-    {% begin %}
-      {% a = [1, 2, 3, 4, 5] %}
-      {% for l in a %}
-        {% for r in a %}
-          it_merges({{l}}, {{r}}, Visitor::ArrayLogic) do |i, j|
-            create_network(i, j)
-          end
-        {% end %}
-      {% end %}
-    {% end %}
+describe Scheme::FlexibleCombineFromPw2Combine do
+  a = [1, 2, 3, 4, 5]
+  a.each do |left|
+    a.each do |right|
+      it_merges(left, right, Visitor::ArrayLogic) do |i, j|
+        create_network(i, j)
+      end
+    end
   end
 end
