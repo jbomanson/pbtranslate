8a272c876474be1d4051109280b76b90c13ae20c
Bomanson Jori <jori.bomanson@aalto.fi>
2017-12-19 16:37:38 +0200

    Use gate_option_keys in PartialFlexibleVoid spec

--- spec/pbtranslate/scheme/partial_flexible_void_spec.cr
+++ spec/pbtranslate/scheme/partial_flexible_void_spec.cr
@@ -3,34 +3,55 @@ require "../../spec_helper"
 include PBTranslate
 
 private struct DummyScheme
-  Scheme.declare_gate_options
+  include Scheme
+  include Scheme::WithArguments(Nil)
+
+  def network(unused : Nil) : Network
+    Network.singleton("gate")
+  end
 end
 
 private struct DummySchemeWithDepth
-  Scheme.declare_gate_options level
+  include Scheme
+  include Scheme::WithArguments(Nil)
+
+  def network(unused : Nil) : Network
+    Network.singleton("gate", level: Distance.new(0))
+  end
 end
 
+# A SCHEME with a gate type that is the union of the gate types of networks
+# of types *A* and *B*.
 private record DummyUnionScheme(A, B), one : A, two : B do
-  delegate gate_options, to: (true ? @one : @two)
+  include Scheme
+  include Scheme::WithArguments(Nil)
+
+  def network(unused : Nil) : Network
+    [@one, @two].first.network(nil)
+  end
 end
 
-scheme = Scheme.partial_flexible_void
+private SCHEME = Scheme.partial_flexible_void
 
 describe PBTranslate::Scheme::PartialFlexibleVoid do
   it "generates no networks with #network?" do
-    scheme.network?.should be_nil
-    scheme.network?(Width.from_value(Distance.new(0))).should be_nil
+    SCHEME.network?.should be_nil
+    SCHEME.network?(Width.from_value(Distance.new(0))).should be_nil
   end
 
-  it "raises on #gate_options" do
-    expect_raises(ImpossibleError) { scheme.gate_options }
-  end
-
-  it "does not change an empty set of #gate_options in unions" do
-    empty = {level: nil, output_cone: nil}
+  it "does not change an empty set of gate options in unions" do
+    empty = CompileTimeSet.create
     dummy = DummyScheme.new
-    dummy.gate_options.restrict(**empty)
-    typeof(DummyUnionScheme.new(dummy, scheme).gate_options.restrict(**empty))
-    typeof(DummyUnionScheme.new(scheme, dummy).gate_options.restrict(**empty))
+    dummy.gate_option_keys.should eq(empty)
+    DummyUnionScheme.new(dummy, SCHEME).gate_option_keys.should eq(empty)
+    DummyUnionScheme.new(SCHEME, dummy).gate_option_keys.should eq(empty)
+  end
+
+  it "does not change a singleton set of gate options in unions" do
+    level = CompileTimeSet.create(:level)
+    dummy = DummySchemeWithDepth.new
+    dummy.gate_option_keys.should eq(level)
+    DummyUnionScheme.new(dummy, SCHEME).gate_option_keys.should eq(level)
+    DummyUnionScheme.new(SCHEME, dummy).gate_option_keys.should eq(level)
   end
 end
--- src/pbtranslate.cr
+++ src/pbtranslate.cr
@@ -1,7 +1,9 @@
 require "./pbtranslate/command"
 
+require "./pbtranslate/compile_time_set"
 require "./pbtranslate/error"
-require "./pbtranslate/scheme/pw2_merge_direct"
-require "./pbtranslate/scheme/pw2_divide_and_conquer"
+require "./pbtranslate/network/singleton"
 require "./pbtranslate/scheme/flexible_from_pw2"
+require "./pbtranslate/scheme/pw2_divide_and_conquer"
+require "./pbtranslate/scheme/pw2_merge_direct"
 require "./pbtranslate/visitor/array_swap"
