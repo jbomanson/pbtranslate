4407eb23aca051cacd2b55fda008c67d43824eca
Bomanson Jori <jori.bomanson@aalto.fi>
2017-12-20 17:40:25 +0200

    Simplify Network#to_network_with_gate_level spec

--- spec/pbtranslate/level_tracking_spec.cr
+++ spec/pbtranslate/level_tracking_spec.cr
@@ -2,27 +2,20 @@ require "../../spec_helper"
 
 include PBTranslate
 
-private struct MyVisitor
-  include Visitor
-
-  getter array
-
-  @array = Array({Distance, Distance} | {Distance, Distance, Distance}).new
-
-  def visit_gate(gate, memo, level)
-    @array << {level} + gate.wires
-    memo
-  end
-end
-
 describe "Network#to_network_with_gate_level" do
   it "computes levels in a small example network correctly" do
-    e = [{0, 0, 1}, {0, 2, 3}, {1, 0, 2}, {1, 1, 3}, {2, 1, 2}]
-    e = e.map &.map { |v| Distance.new(v) }
-    a = e.map { |(d, i, j)| {i, j} }
-    network = Network.flexible_comparator(a).to_network_with_gate_level
-    visitor = MyVisitor.new
-    network.host(visitor)
-    visitor.array.select { |t| t.size == 3 }.should eq(e)
+    gates_with_options = [
+      {Gate.comparator_between(Distance.new(0), Distance.new(1)), {level: Distance.new(0)}},
+      {Gate.comparator_between(Distance.new(2), Distance.new(3)), {level: Distance.new(0)}},
+      {Gate.comparator_between(Distance.new(0), Distance.new(2)), {level: Distance.new(1)}},
+      {Gate.comparator_between(Distance.new(1), Distance.new(3)), {level: Distance.new(1)}},
+      {Gate.comparator_between(Distance.new(1), Distance.new(2)), {level: Distance.new(2)}},
+    ]
+    Network
+      .flexible_comparator(gates_with_options.map(&.first.wires))
+      .to_network_with_gate_level
+      .gates_with_options
+      .to_a
+      .should eq(gates_with_options)
   end
 end
