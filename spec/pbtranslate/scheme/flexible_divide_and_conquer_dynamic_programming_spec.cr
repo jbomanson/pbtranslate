378e2af04ece64c0732548ceff44a463e1059527
Bomanson Jori <jori.bomanson@aalto.fi>
2018-01-04 13:48:58 +0200

    In FlexibleDivideAndConquerDynamicProgramming spec use globals

--- spec/pbtranslate/scheme/flexible_divide_and_conquer_dynamic_programming_spec.cr
+++ spec/pbtranslate/scheme/flexible_divide_and_conquer_dynamic_programming_spec.cr
@@ -2,19 +2,19 @@ require "../sorting_network_helper"
 
 include SpecHelper
 
-network_count = 10
-scheme =
+private NETWORK_COUNT = 10
+private SCHEME        =
   Scheme
     .pw2_merge_odd_even
     .to_scheme_flexible_combine
     .to_scheme_flexible_divide_and_conquer_dynamic_programming
     .to_scheme_with_offset_resolution
-private SEED = SpecHelper.file_specific_seed
-random = Random.new(SEED)
-max = 128
-range = array_of_random_width(network_count, random, max: max).map { |v| Width.from_value(v) }
-rounds = 5
+private SEED   = SpecHelper.file_specific_seed
+private RANDOM = Random.new(SEED)
+private MAX    = 128
+private RANGE  = array_of_random_width(NETWORK_COUNT, RANDOM, max: MAX).map { |v| Width.from_value(v) }
+private ROUNDS = 5
 
 describe Scheme::FlexibleDivideAndConquerDynamicProgramming do
-  it_hosts_like_a_sorting_network(scheme, SEED, range, rounds)
+  it_hosts_like_a_sorting_network(SCHEME, SEED, RANGE, ROUNDS)
 end
